12a5dc1a65924b33bece143d37f97a9540eaec74
husni <husni@localhost.localdomain>
2021-10-22 10:19:50 +0300

    Initial commit after change for submission

--- data.js
+++ data.js
@@ -1,18 +1,7 @@
 module.exports = [
   {
-    title: "archive/fagdag/instagram",
-    path: "archive/fagdag/instagram.js",
+    title: "archive/husninazer/story",
+    path: "archive/husninazer/story.js",
     ignore: true,
-  },
-  {
-    title: "archive/fagdag/story",
-    path: "archive/fagdag/story.js",
-    ignore: true,
-  },
-  {
-    title: "archive/fagdag/web",
-    path: "archive/fagdag/web.js",
-    ignore: true,
-    dynamic: true,
   },
 ];
